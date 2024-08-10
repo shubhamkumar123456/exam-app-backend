ff56b3fefa8d1816bd5e74a17272b234a8ef12c8
shubham kumar <clboy768@gmail.com>
2024-08-10 14:43:26 +0530

    updated env

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 const connectToDb= ()=>{
-    mongoose.connect('mongodb://localhost:27017/exam1')
+    mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/exam1')
     .then(()=>console.log('Connected to database successfully'))
     .catch((error)=>console.log("failed to connect to database",error))
 }
