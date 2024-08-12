0cbfbadc7eb49513011c5f9f75cb11ff960da1e9
shubham kumar <clboy768@gmail.com>
2024-08-13 01:13:30 +0530

    exam controller

--- controllers/exam.js
+++ controllers/exam.js
@@ -1,84 +1,86 @@
 const Exam = require('../models/Exam')
 
-const createExam = async(req,res)=>{
-    const {examName,batch,category} = req.body;
-    const exam = await Exam.create(req.body)
-    try {
-       return res.status(200).json({msg:"exam created successfully",exam,success:true})
-    } catch (error) {
-        return res.status(500).json({msg:"error in creating exam",error,success:false})
-    }
-}
-
-const AddQuestion = async(req, res)=>{
-    let exam = await Exam.findOne({_id:req.params.examId})
-    let question = req.body.question
-    // res.json(exam)
+const createExam = async (req, res) => {
+  const { examName, batch, category } = req.body;
+  const exam = await Exam.create(req.body)
   try {
-    if(exam){
-       let updatedExam= await Exam.updateOne({ _id: exam }, { $push: {question} },{new:true});
-        // db.people.update({name: "John"}, {$push: {friends: {firstName: "Harry", lastName: "Potter"}}});
-        return res.status(200).json({msg:"question added to exam successfully",updatedExam,success:true})
-     }
+    return res.status(200).json({ msg: "exam created successfully", exam, success: true })
   } catch (error) {
-    return res.status(500).json({msg:"error in adding question exam",error,success:false})
+    return res.status(500).json({ msg: "error in creating exam", error, success: false })
   }
 }
-const getSingleExam = async(req,res)=>{
-    let exam = await Exam.findById(req.params._id).populate([{ 
-        path: 'question',
-        populate: {
-          path: 'options',
-          model: 'Option'
-        } 
-     },
-     { 
-        path: 'question',
-        populate: {
-          path: 'correctOption',
-          model: 'Option'
-        } 
-     }
-    ])
-    try {
-       return res.status(200).json({msg:"fetched successfully",exam,success:true})
-    } catch (error) {
-        return res.status(500).json({msg:"error in adding question exam",error,success:false})
+
+const AddQuestion = async (req, res) => {
+  let exam = await Exam.findOne({ _id: req.params.examId })
+  let question = req.body.question
+  // res.json(exam)
+  try {
+    if (exam) {
+      let updatedExam = await Exam.updateOne({ _id: exam }, { $push: { question } }, { new: true });
+      // db.people.update({name: "John"}, {$push: {friends: {firstName: "Harry", lastName: "Potter"}}});
+      return res.status(200).json({ msg: "question added to exam successfully", updatedExam, success: true })
     }
+  } catch (error) {
+    return res.status(500).json({ msg: "error in adding question exam", error, success: false })
+  }
+}
+const getSingleExam = async (req, res) => {
+  let exam = await Exam.findById(req.params._id).populate([{
+    path: 'question',
+    populate: {
+      path: 'options',
+      model: 'Option'
+    }
+  },
+  {
+    path: 'question',
+    populate: {
+      path: 'correctOption',
+      model: 'Option'
+    }
+  }
+  ])
+  try {
+    return res.status(200).json({ msg: "fetched successfully", exam, success: true })
+  } catch (error) {
+    return res.status(500).json({ msg: "error in adding question exam", error, success: false })
+  }
 }
 
-const getAllexam = async(req,res)=>{
-    const exam = await Exam.find({}).populate([{ 
-      path: 'question',
-      populate: {
-        path: 'options',
-        model: 'Option'
-      } 
-   },
-   { 
-      path: 'question',
-      populate: {
-        path: 'correctOption',
-        model: 'Option'
-      } 
-   }
+const getAllexam = async (req, res) => {
+  const exam = await Exam.find({}).populate([{
+    path: 'question',
+    populate: {
+      path: 'options',
+      model: 'Option'
+    }
+  },
+  {
+    path: 'question',
+    populate: {
+      path: 'correctOption',
+      model: 'Option'
+    }
+  }
   ]);
-    return res.status(200).json({msg:"fetched all exam successfully",exam,success:true})
+  return res.status(200).json({ msg: "fetched all exam successfully", exam, success: true })
 }
 
-const toggleExamEnable =async(req,res)=>{
+const toggleExamEnable = async (req, res) => {
   let examId = req.params.examId;
-  let enable=req.body.enable;
-  let exam = await Exam.findByIdAndUpdate(examId,{$set:{enable:enable}},{new:true});
-  res.json({msg:"exam switched succeffully",exam})
-  
+  let enable = req.body.enable;
+  console.log(enable);
+  console.log(examId);
+  let exam = await Exam.findByIdAndUpdate(examId, { $set: { enable: enable } }, { new: true });
+  res.json({ msg: "exam switched succeffully", exam })
+
 }
 
 
-module.exports ={
-    createExam,
-    AddQuestion,
-    getAllexam,
-    getSingleExam,
-    toggleExamEnable
+module.exports = {
+  createExam,
+  AddQuestion,
+  getAllexam,
+  getSingleExam,
+  toggleExamEnable
 }
